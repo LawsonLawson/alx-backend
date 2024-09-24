56982a78425b87484fbd6c5e7e0c83e7c47691c3
Lawson Israel Pascal <isreallawson@outlook.com>
2024-09-24 18:35:33 +0000

    semistandard fix

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -13,12 +13,12 @@ const redisConnect = () => {
   client.on('error', err => {
     console.log('Redis client not connected to the server: ', err);
   });
-  
+
   // Listen for successful connection to the Redis server
   client.on('connect', () => {
     console.log('Redis client connected to the server');
   });
-}
+};
 
 // Call the redisConnect function to set up the listeners
 redisConnect();
